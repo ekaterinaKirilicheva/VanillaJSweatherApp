7b02bcf8bd093c017d2a0721d55739cb8b245c9f
Ekaterina <kirilichevae@gmail.com>
2022-04-12 20:53:43 +0200

    added a weather icon

--- src/index.js
+++ src/index.js
@@ -51,6 +51,12 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchLocation(position) {
